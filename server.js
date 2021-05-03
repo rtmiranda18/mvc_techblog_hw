ac5839fd22cffcbc07c1351e994f0914b6aa1c48
Reesh Miranda <rtmiranda18@gmail.com>
2021-05-03 20:55:54 +0800

    input code on server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/dashboard', function (req, res) {
+    res.render('dashboard');
+})
+
+app.listen(3000);
