9f1dfe600ebd9939332a450d511202116aff69c5
Reesh Miranda <rtmiranda18@gmail.com>
2021-05-05 18:02:00 +0800

    edited models

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 let Sequelize = require('sequelize');
 let bcrypt = require('bcrypt');
 
-const sequelize = new Sequelize('cms', 'root', 'password', {
+const sequelize = new Sequelize('techblog_db', 'root', 'password', {
     host: 'localhost',
     port: 3306,
     dialect: 'mysql'
